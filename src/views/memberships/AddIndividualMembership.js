bf555b5c84ec5599e4665a033d025bf0065212e0
umairahmed17 <umair.ahmad1257@gmail.com>
2022-05-15 12:11:23 +0500

    fix: Email Regex Change
    The previous regex was not proper

--- src/views/memberships/AddIndividualMembership.js
+++ src/views/memberships/AddIndividualMembership.js
@@ -121,7 +121,8 @@ class AddIndividualMembership extends React.Component {
 	};
 
 	validateEmail(e) {
-		const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
+		const emailRegex =
+			/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 		const { validate } = this.state;
 
